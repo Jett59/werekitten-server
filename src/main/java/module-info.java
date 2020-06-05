994f915cf567788ab9a020ff8ca5fb5997d2ef32
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-05 16:20:41 +1000

    removed the module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycodefu.werekitten.server {
-    requires io.netty.common;
-    requires io.netty.handler;
-    requires io.netty.codec.http;
-    requires io.netty.codec;
-    requires io.netty.transport;
-    requires io.netty.buffer;
-	
-}
