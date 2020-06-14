5b564877fa48103718db15f07b927c6043166a4c
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-14 19:46:13 +1000

    added some chmods to the install and deploy scripts

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ rm -rf werekitten-server
 mkdir werekitten-server
 cd werekitten-server
 unzip /home/ec2-user/werekitten-server.zip
+chmod +x shutdown.sh
 ./shutdown.sh
+chmod +x startup.sh
 ./startup.sh
