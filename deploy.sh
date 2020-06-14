a065bfc78ff26c139c245f588b16a5a7e320a57b
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-14 19:01:09 +1000

    attempted to add the full deploy to the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,13 @@ chmod 400 server.pem
 
 ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "rm -f werekitten-server.zip"
 scp -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no target/release-directory/werekitten-server.zip ec2-user@werekitten.mycodefu.com:
+
+ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com << EOF
+  cd /opt
+  rm -rf werekitten-server
+  mkdir werekitten-server
+  cd werekitten-server
+  unzip /home/ec2-user/werekitten-server.zip
+  ./shutdown.sh
+  ./startup.sh
+EOF
