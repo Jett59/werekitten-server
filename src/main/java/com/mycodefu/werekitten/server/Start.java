964728701971784b09ce243babe234d69bc658f4
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-14 17:05:43 +1000

    added a shutDownHook

--- src/main/java/com/mycodefu/werekitten/server/Start.java
+++ src/main/java/com/mycodefu/werekitten/server/Start.java
@@ -1,13 +1,19 @@
 package com.mycodefu.werekitten.server;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import com.mycodefu.werekitten.server.netty.NettyServer;
 import com.mycodefu.werekitten.server.world.WorldManager;
 
 public class Start {
 public static void main(String[] args) {
+	AtomicReference<NettyServer> server = new AtomicReference<>();
 	WorldManager manager = new WorldManager();
-	NettyServer server = new NettyServer(51273, manager);
-	manager.setServer(server);
-	server.listen();
+	server.set(new NettyServer(51273, manager));
+	manager.setServer(server.get());
+	server.get().listen();
+	Runtime.getRuntime().addShutdownHook(new Thread(()-> {
+		server.get().close();
+	}));
 }
 }
