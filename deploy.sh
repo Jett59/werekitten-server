88f9b91f6e73dd968c22c13b12d6c69597d7af5f
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-14 17:31:32 +1000

    added an upload to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 echo "Building the deployable artifact ./target/release-directory/werekitten-server.zip"
 mvn package
-
+echo "${SERVER_PEM}">server.pem
+chmod 400 server.pem
+scp -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no target/release-directory/werekitten-server.zip ec2-user@werekitten.mycodefu.com:
