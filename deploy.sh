ec813470f91e291b8f8d5528dee46bac255d103e
Jett Thompson <jett.thompson.59@gmail.com>
2020-06-14 21:12:00 +1000

    added sudo to server commands when required

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ chmod 400 server.pem
 ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "rm -f werekitten-server.zip"
 scp -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no target/release-directory/werekitten-server.zip ec2-user@werekitten.mycodefu.com:
 scp -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no install.sh ec2-user@werekitten.mycodefu.com:
-ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "chmod +x install.sh"
-ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "./install.sh"
+ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "sudo chmod +x install.sh"
+ssh -i server.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@werekitten.mycodefu.com "sudo ./install.sh"
